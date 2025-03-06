da71d361fade75d117c88d6cb9a73abb71fdbb8e
Masterrumtat <captain019377@gmail.com>
2025-03-06 22:21:53 +0700

    Initial Commit

---
+++ FitFriend/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FitFriend
+//
+//  Created by Vituruch Sinthusate on 6/3/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FitFriend/FitFriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitFriendApp.swift
+//  FitFriend
+//
+//  Created by Vituruch Sinthusate on 6/3/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct FitFriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
